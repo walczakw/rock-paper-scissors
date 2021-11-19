6681e26ae89ce2056f57e8c4157d7a2967086c1b
Wiola <walczakw864@gmail.com>
2021-11-19 15:59:30 +0100

    Add variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var computermove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
